bf78dde1c113dd9771c795dd14035e9e6360272f
Michael-Steshenko <19752384+Michael-Steshenko@users.noreply.github.com>
2019-10-05 19:29:33 +0300

    Create boot_to_raspbian.sh

---
+++ boot_to_raspbian.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# mounts the partition on which pinn is installed
+sudo mount /dev/mmcblk0p1 /media/pinn
+cd /media/pinn
+sudo cp autoboot_raspbian.txt autoboot.txt
+cd
+sudo umount /media/pinn
+sudo killall emulationstation; sudo reboot
