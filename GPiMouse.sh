e1840187c2cf8de10c150a8a46d904aca5fe5870
Michael Steshenko <19752384+Michael-Steshenko@users.noreply.github.com>
2020-03-04 06:26:50 +0200

    Update GPiMouse.sh

--- GPiMouse.sh
+++ GPiMouse.sh
@@ -10,4 +10,4 @@
 # select is escape
 
 sudo rmmod xpad
-sudo xboxdrv --ui-buttonmap "dr=REL_X:5:20,dl=REL_X:-5:20,du=REL_Y:-5:20,dd=REL_Y:5:20," --ui-buttonmap "a=BTN_LEFT,b=BTN_RIGHT,x=BTN_MIDDLE,y=KEY_ENTER,rb=KEY_DOWN,lb=KEY_UP," --ui-buttonmap "start=KEY_FORWARD,back=KEY_ESC,tl=void,tr=void"
+sudo xboxdrv --silent --ui-buttonmap "dr=REL_X:5:20,dl=REL_X:-5:20,du=REL_Y:-5:20,dd=REL_Y:5:20," --ui-buttonmap "a=BTN_LEFT,b=BTN_RIGHT,x=BTN_MIDDLE,y=KEY_ENTER,rb=KEY_DOWN,lb=KEY_UP," --ui-buttonmap "start=KEY_FORWARD,back=KEY_ESC,tl=void,tr=void" &
