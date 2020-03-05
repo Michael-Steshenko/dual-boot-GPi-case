e92c8bc5ba5faa709d568e027b78a4430849a839
Michael Steshenko <19752384+Michael-Steshenko@users.noreply.github.com>
2020-03-05 04:54:57 +0200

    Update boot_raspbian.sh

--- boot_raspbian.sh
+++ boot_raspbian.sh
@@ -6,5 +6,4 @@ cd /media/pinn
 sudo cp autoboot_raspbian.txt autoboot.txt
 cd
 sudo umount /media/pinn
-# for some reason reboot messes up GPi keys control on the second partition but shutdown now -r works
 sudo killall emulationstation; sudo shutdown now -r
