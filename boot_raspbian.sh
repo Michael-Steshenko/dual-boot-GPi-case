b11f732d380de9debc25591a89f0e851c575c57d
Michael Steshenko <19752384+Michael-Steshenko@users.noreply.github.com>
2020-03-05 03:23:10 +0200

    fixed controls bug caused by OS switch scripts
    for some reason reboot messes up GPi keys control on the second partition but shutdown now -r works

--- boot_raspbian.sh
+++ boot_raspbian.sh
@@ -6,4 +6,5 @@ cd /media/pinn
 sudo cp autoboot_raspbian.txt autoboot.txt
 cd
 sudo umount /media/pinn
-sudo killall emulationstation; sudo reboot
+# for some reason reboot messes up GPi keys control on the second partition but shutdown now -r works
+sudo killall emulationstation; sudo shutdown now -r
